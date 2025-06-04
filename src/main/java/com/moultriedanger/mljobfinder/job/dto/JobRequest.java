1e845cdb83c1a248931da83c4af45b1a8739b32f
Moultrie Dangerfield <moultriedanger@gmail.com>
2025-06-04 19:17:28 -0400

    added jobRequest DTO

--- src/main/java/com/moultriedanger/mljobfinder/job/dto/JobRequest.java
+++ src/main/java/com/moultriedanger/mljobfinder/job/dto/JobRequest.java
@@ -1,4 +1,93 @@
 package com.moultriedanger.mljobfinder.job.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+
 public class JobRequest {
+
+    @NotBlank(message = "Job title is required")
+    private String jobTitle;
+
+    @NotBlank(message = "Job description is required")
+    private String jobDescription;
+
+    @NotBlank(message = "Job seniority is required")
+    private String seniorityLevel;
+
+    @NotBlank(message = "Job salary is required")
+    private String maxSalary;
+
+    @NotBlank(message = "Job location is required")
+    private String location;
+
+    @NotBlank(message = "Job url is required")
+    private String postingUrl;
+
+    @NotNull(message = "Company ID is required")
+    private Long companyId;
+
+    public JobRequest() {}
+
+    public JobRequest(String jobTitle, String jobDescription, String seniorityLevel, String maxSalary, String location, String postingUrl, Long companyId) {
+        this.jobTitle = jobTitle;
+        this.jobDescription = jobDescription;
+        this.seniorityLevel = seniorityLevel;
+        this.maxSalary = maxSalary;
+        this.location = location;
+        this.postingUrl = postingUrl;
+        this.companyId = companyId;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public String getJobDescription() {
+        return jobDescription;
+    }
+
+    public String getSeniorityLevel() {
+        return seniorityLevel;
+    }
+
+    public String getMaxSalary() {
+        return maxSalary;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getPostingUrl() {
+        return postingUrl;
+    }
+
+    public Long getCompanyId(){ return companyId; }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public void setJobDescription(String jobDescription) {
+        this.jobDescription = jobDescription;
+    }
+
+    public void setSeniorityLevel(String seniorityLevel) {
+        this.seniorityLevel = seniorityLevel;
+    }
+
+    public void setMaxSalary(String maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setPostingUrl(String postingUrl) {
+        this.postingUrl = postingUrl;
+    }
+
+    public void setCompanyId(Long companyId){ this.companyId = companyId; }
 }
