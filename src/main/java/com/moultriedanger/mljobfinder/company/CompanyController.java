27793d66b9e249acb38dfa0df26ad0509bda0165
Moultrie Dangerfield <moultriedanger@gmail.com>
2025-06-06 10:14:05 -0400

    added CORS support

--- src/main/java/com/moultriedanger/mljobfinder/company/CompanyController.java
+++ src/main/java/com/moultriedanger/mljobfinder/company/CompanyController.java
@@ -4,6 +4,7 @@ import com.moultriedanger.mljobfinder.company.dto.CompanyResponse;
 import com.moultriedanger.mljobfinder.job.Job;
 import com.moultriedanger.mljobfinder.job.dto.JobResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class CompanyController {
 
