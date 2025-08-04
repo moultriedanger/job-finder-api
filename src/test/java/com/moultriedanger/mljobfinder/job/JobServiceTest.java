981094030e6d207bf50e34a55facabf7b31104ee
Moultrie Dangerfield <moultriedanger@gmail.com>
2025-08-04 17:23:41 -0400

    Configure H2 & create-drop for all test slices

--- src/test/java/com/moultriedanger/mljobfinder/job/JobServiceTest.java
+++ src/test/java/com/moultriedanger/mljobfinder/job/JobServiceTest.java
@@ -3,12 +3,15 @@ package com.moultriedanger.mljobfinder.job;
 import com.moultriedanger.mljobfinder.company.CompanyRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 
 @DataJpaTest
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class JobServiceTest {
 
     @Autowired
