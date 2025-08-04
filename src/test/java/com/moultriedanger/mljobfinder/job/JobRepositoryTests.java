50ef81f3787ce406add909871b10feab02a94937
Moultrie Dangerfield <moultriedanger@gmail.com>
2025-08-04 17:15:26 -0400

    use test profile on tests

--- src/test/java/com/moultriedanger/mljobfinder/job/JobRepositoryTests.java
+++ src/test/java/com/moultriedanger/mljobfinder/job/JobRepositoryTests.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
@@ -17,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
+@ActiveProfiles("test")
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class JobRepositoryTests {
 
--- src/test/java/com/moultriedanger/mljobfinder/job/JobServiceTest.java
+++ src/test/java/com/moultriedanger/mljobfinder/job/JobServiceTest.java
@@ -4,8 +4,11 @@ import com.moultriedanger.mljobfinder.company.CompanyRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @DataJpaTest
+@ActiveProfiles("test")
 public class JobServiceTest {
 
     @Autowired
