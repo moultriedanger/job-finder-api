3263f3f4eca157d5f166af21175056d13814190c
Moultrie Dangerfield <moultriedanger@gmail.com>
2025-08-16 16:05:08 -0400

    added dto classes for login, registry, and verify

---
+++ src/main/java/com/moultriedanger/mljobfinder/user/dto/LoginUserDto.java
@@ -0,0 +1,12 @@
+package com.moultriedanger.mljobfinder.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginUserDto {
+
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/moultriedanger/mljobfinder/user/dto/RegisterUserDto.java
@@ -0,0 +1,17 @@
+package com.moultriedanger.mljobfinder.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterUserDto {
+
+    private String email;
+
+    private String password;
+
+    private String username;
+
+
+}
