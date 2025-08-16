56d9d2a60eb7f368e095205d04d020ba3449193f
Moultrie Dangerfield <moultriedanger@gmail.com>
2025-08-16 12:43:56 -0400

    added user repository

--- src/main/java/com/moultriedanger/mljobfinder/user/UserRepository.java
+++ src/main/java/com/moultriedanger/mljobfinder/user/UserRepository.java
@@ -1,15 +1,13 @@
 package com.moultriedanger.mljobfinder.user;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.moultriedanger.mljobfinder.user.User;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<User> findByUsername(String username);
-    boolean existsByUsername(String username);
-    boolean existsByEmail(String email);
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationCode(String verificationCode);
 }
-
